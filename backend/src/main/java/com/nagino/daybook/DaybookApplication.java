3e6c2163535a5ab98769f066b05fe984241ce0d3
Nagino <dawnpioneer1989@gmail.com>
2021-09-26 17:40:12 +0800

    create spring boot project from 'start.spring.io'

---
+++ backend/src/main/java/com/nagino/daybook/DaybookApplication.java
@@ -0,0 +1,13 @@
+package com.nagino.daybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaybookApplication.class, args);
+	}
+
+}
