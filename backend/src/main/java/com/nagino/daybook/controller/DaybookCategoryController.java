bda9a98fd6ccc8352c51b3cf3c9ceea45c34b020
Nagino <dawnpioneer1989@gmail.com>
2022-01-16 17:38:07 +0800

    1. change server port from 8080 to 8088 2. getAllDaybookCategories order by sortNum 3. auto set sortNum(existed max sortNum + 1) from createDaybookCategory

--- backend/src/main/java/com/nagino/daybook/controller/DaybookCategoryController.java
+++ backend/src/main/java/com/nagino/daybook/controller/DaybookCategoryController.java
@@ -15,6 +15,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -25,47 +26,44 @@ public class DaybookCategoryController extends BaseController {
 
     @GetMapping("/daybookCategories")
     public ResponseEntity<List<DaybookCategory>> getAllDaybookCategories(@RequestParam(required = false) String category, String name) {
-        Specification<DaybookCategory> specification = new Specification<DaybookCategory>(){
-            @Override
-            public Predicate toPredicate(Root<DaybookCategory> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                List<Predicate> predicatesList = new ArrayList<>();
-                // equal ownerId
-                if (getCurrentUserId() != null){
-                    Predicate namePredicate = cb.equal(root.get("ownerId"), getCurrentUserId());
-                    predicatesList.add(namePredicate);
-                }
-                // equal category
-                if (!StringUtils.isEmpty(category)){
-                    Predicate namePredicate = cb.equal(root.get("category"), category);
-                    predicatesList.add(namePredicate);
-                }
-                // like %name%
-                if (!StringUtils.isEmpty(name)){
-                    Predicate nickNamePredicate = cb.like(root.get("name"), '%'+name+'%');
-                    predicatesList.add(nickNamePredicate);
-                }
-                Predicate[] predicates = new Predicate[predicatesList.size()];
-                return cb.and(predicatesList.toArray(predicates));
+        Specification<DaybookCategory> specification = (root, query, cb) -> {
+            List<Predicate> predicatesList = new ArrayList<>();
+            // equal ownerId
+            if (getCurrentUserId() != null){
+                Predicate namePredicate = cb.equal(root.get("ownerId"), getCurrentUserId());
+                predicatesList.add(namePredicate);
             }
+            // equal category
+            if (!StringUtils.isEmpty(category)){
+                Predicate namePredicate = cb.equal(root.get("category"), category);
+                predicatesList.add(namePredicate);
+            }
+            // like %name%
+            if (!StringUtils.isEmpty(name)){
+                Predicate nickNamePredicate = cb.like(root.get("name"), '%'+name+'%');
+                predicatesList.add(nickNamePredicate);
+            }
+            Predicate[] predicates = new Predicate[predicatesList.size()];
+            query.orderBy(cb.asc(root.get("sortNum")));
+            return cb.and(predicatesList.toArray(predicates));
         };
         return new ResponseEntity<>(daybookCategoryRepository.findAll(specification), HttpStatus.OK);
     }
 
     @GetMapping("/daybookCategories/{id}")
     public ResponseEntity<DaybookCategory> getDaybookCategoryById(@PathVariable("id") long id) {
-        Optional<DaybookCategory> DaybookCategoryData = daybookCategoryRepository.findByIdAndOwnerId(id, getCurrentUserId());
+        Optional<DaybookCategory> daybookCategoryData = daybookCategoryRepository.findByIdAndOwnerId(id, getCurrentUserId());
 
-        if (DaybookCategoryData.isPresent()) {
-            return new ResponseEntity<>(DaybookCategoryData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return daybookCategoryData.map(daybookCategory -> new ResponseEntity<>(daybookCategory, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/daybookCategories")
     public ResponseEntity<DaybookCategory> createDaybookCategory(@RequestBody DaybookCategory daybookCategory) {
         try {
             DaybookCategory newDaybookCategory = new DaybookCategory(daybookCategory.getCategory(), daybookCategory.getName(), getCurrentUserId(), daybookCategory.getSortNum());
+            ResponseEntity<List<DaybookCategory>> daybookCategories = getAllDaybookCategories(null, null);
+            // 新增時的sortNum預設為目前資料最大的sortNum+1
+            newDaybookCategory.setSortNum(Objects.requireNonNull(daybookCategories.getBody()).get(daybookCategories.getBody().size()-1).getSortNum() + 1);
             DaybookCategory _daybookCategory = daybookCategoryRepository.save(newDaybookCategory);
             return new ResponseEntity<>(_daybookCategory, HttpStatus.CREATED);
         } catch (Exception e) {
